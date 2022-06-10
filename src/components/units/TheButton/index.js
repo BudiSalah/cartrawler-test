7991a7667f96232f40b4e5473bea9f44a7ad46d5
Budi-Laptop <budi.salah@outlook.com>
2022-06-10 16:51:12 +0200

    create TheButton component

---
+++ src/components/units/TheButton/index.js
@@ -0,0 +1,14 @@
+import style from "./style.module.scss";
+
+function TheButton(props) {
+  const btnClasses = [
+    style.btn,
+    props.type === "primary" && style["btn--primary"],
+    props.type === "secondary" && style["btn--secondary"],
+    props.type === "disabled" && style["btn--disabled"],
+  ].join(" ");
+
+  return <button className={`${btnClasses} body-l capitalize`}>primary</button>;
+}
+
+export default TheButton;
