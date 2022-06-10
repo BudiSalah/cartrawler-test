248885e214c48cc55c3cda8df88aa7b29422cced
Budi-Laptop <budi.salah@outlook.com>
2022-06-11 00:59:29 +0200

    apply AppProvider on App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.scss";
+import AppProvider from "./components/AppProvider";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} exact />
-      </Routes>
-    </Router>
+    <AppProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+        </Routes>
+      </Router>
+    </AppProvider>
   );
 }
 
