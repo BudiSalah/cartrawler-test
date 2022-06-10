79c3938518e957a95440ffde01019aca114e5e2b
Budi-Laptop <budi.salah@outlook.com>
2022-06-11 01:00:39 +0200

    apply initial AppContext on Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+import AppContext from "../../store/AppContext";
 import LayoutMain from "../../layouts/LayoutMain";
 import CarCard from "../../components/CarCard";
 
 function Home() {
+  const { nameHandler, name } = useContext(AppContext);
+
   return (
     <LayoutMain>
-      <CarCard></CarCard>
+      <CarCard />
+
+      <h1 onClick={nameHandler}>{name}</h1>
     </LayoutMain>
   );
 }
