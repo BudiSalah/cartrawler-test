142a2c17a4d1fd3e32ebee2a533f9f4760623fc3
Budi-Laptop <budi.salah@outlook.com>
2022-06-11 05:11:45 +0200

    set props and vendor components for API integration in CarCard

--- src/components/CarCard/index.js
+++ src/components/CarCard/index.js
@@ -11,16 +11,39 @@ import SvgAvis from "./../svg/SvgAvis";
 import SvgHertz from "./../svg/SvgHertz";
 import SvgAlamo from "./../svg/SvgAlamo";
 
-function CarCard() {
-  const Vendor = SvgAvis || SvgHertz || SvgAlamo;
+function CarCard(props) {
+  const Vendor = () => {
+    return (
+      {
+        ALAMO: <SvgAlamo />,
+        AVIS: <SvgAvis />,
+        HERTZ: <SvgHertz />,
+      }[props?.vendor?.["@Name"]] || ""
+    );
+  };
+
+  const vehicle = props?.vehicleData?.Vehicle;
+
+  function getCurrencySymbol(currency) {
+    return (0)
+      .toLocaleString(navigator.language, {
+        style: "currency",
+        currency: currency,
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      })
+      .replace(/\d/g, "")
+      .trim();
+  }
+
+  const currencySymbol = getCurrencySymbol(
+    props?.vehicleData?.TotalCharge["@CurrencyCode"]
+  );
 
   return (
     <LayoutCard>
       <figure className={`${style["figure-car"]}`}>
-        <img
-          src="https://ctimg-fleet.cartrawler.com/toyota/rav4/primary.png?auto=format&w=600"
-          alt="xx"
-        />
+        <img src={vehicle?.PictureURL} alt={vehicle?.VehMakeModel?.["@Name"]} />
       </figure>
 
       <div className={`${style.content} flex flex-col justify-between`}>
@@ -31,54 +54,83 @@ function CarCard() {
         <hr style={{ marginTop: "10px" }} />
 
         <div className={`${style["car-meta"]} flex flex-row items-center`}>
-          <h2 className="subtitle-xl capitalize">the car name</h2>
+          <h2 className="subtitle-xl capitalize">
+            {vehicle?.VehMakeModel?.["@Name"]}
+          </h2>
         </div>
 
-        <p className={`${style.price} subtitle-m`}>$400</p>
+        <p className={`${style.price} subtitle-m`}>
+          {currencySymbol}
+          {props?.vehicleData?.TotalCharge["@RateTotalAmount"]}
+        </p>
 
         <div
           className={`${style.info} flex flex-row justify-between items-center`}
         >
           <div
+            title="Air Condition"
             className={`${style.info__item} flex flex-row items-center body-l`}
           >
             <SvgSnowflake />
-            <p className="capitalize">info about car</p>
+            <p className="capitalize">
+              <span className="font-semi-bold">Air Condition: </span>
+              {vehicle["@AirConditionInd"] ? "yes" : "no"}
+            </p>
           </div>
 
           <div
+            title="Baggage Quantity"
             className={`${style.info__item} flex flex-row items-center body-l`}
           >
             <SvgBag />
-            <p className="capitalize">info about car</p>
+            <p className="capitalize">
+              <span className="font-semi-bold">Baggage Quantity: </span>
+              {vehicle["@BaggageQuantity"]}
+            </p>
           </div>
 
           <div
+            title="Door Count"
             className={`${style.info__item} flex flex-row items-center body-l`}
           >
             <SvgDoor />
-            <p className="capitalize">info about car</p>
+            <p className="capitalize">
+              <span className="font-semi-bold">Door Count: </span>
+              {vehicle["@DoorCount"]}
+            </p>
           </div>
 
           <div
+            title="Fuel Type"
             className={`${style.info__item} flex flex-row items-center body-l`}
           >
             <SvgFuel />
-            <p className="capitalize">info about car</p>
+            <p className="capitalize">
+              <span className="font-semi-bold">Fuel Type: </span>
+              {vehicle["@FuelType"]}
+            </p>
           </div>
 
           <div
+            title="Passenger Quantity"
             className={`${style.info__item} flex flex-row items-center body-l`}
           >
             <SvgPerson />
-            <p className="capitalize">info about car</p>
+            <p className="capitalize">
+              <span className="font-semi-bold">Passenger Quantity: </span>
+              {vehicle["@PassengerQuantity"]}
+            </p>
           </div>
 
           <div
+            title="Transmission Type"
             className={`${style.info__item} flex flex-row items-center body-l`}
           >
             <SvgTransmission />
-            <p className="capitalize">info about car</p>
+            <p className="capitalize">
+              <span className="font-semi-bold">Transmission Type: </span>
+              {vehicle["@TransmissionType"]}
+            </p>
           </div>
         </div>
 
